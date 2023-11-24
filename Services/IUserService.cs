2a3b066f8caf4e0666c47fc3eee3ca15a8f32ebb
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-24 16:36:51 +0500

    sas

---
+++ Services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using FileWwwroot.Api.Dtos;
+
+namespace FileWwwroot.Api.Services
+{
+    public interface IUserService
+    {
+        public ValueTask<int> CreateAsync(UserDto userDto);
+        public ValueTask<UserResDto> GetByIdAsync(int UserId);
+        public ValueTask<IEnumerable<UserResDto>> GetAllAsync();
+    }
+}
