784f0b744c328dfd87a61710427de276f168e36b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-24 19:29:15 +0500

    sdada

--- Program.cs
+++ Program.cs
@@ -21,11 +21,11 @@ builder.Services.AddScoped<IUserService, UserService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
