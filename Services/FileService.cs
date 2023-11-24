9e840e5e7eea22d084cfedd3b2a5c62aad30cc1f
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-24 16:36:39 +0500

    sas

---
+++ Services/FileService.cs
@@ -0,0 +1,34 @@
+﻿
+using FileWwwroot.Api.Data;
+using Microsoft.AspNetCore.Http;
+
+namespace FileWwwroot.Api.Services
+{
+    public class FileService : IFileService
+    {
+        private readonly FileImageDBContext _dbContext;
+        private readonly IWebHostEnvironment _environment;
+
+        public FileService(IWebHostEnvironment environment, FileImageDBContext dbContext)
+        {
+            _environment = environment;
+            _dbContext = dbContext;
+        }
+
+        public async ValueTask<string> CreateFileAsync(IFormFile filePath)
+        {
+            string extention = Path.GetExtension(filePath.FileName);
+
+            var path = "/Images/" + Guid.NewGuid() + extention;
+
+            string fullPath = _environment.WebRootPath + path;
+
+            using (FileStream stream = new FileStream(fullPath, FileMode.Create))
+            {
+                await filePath.CopyToAsync(stream);
+            }
+
+            return path;
+        }
+    }
+}
